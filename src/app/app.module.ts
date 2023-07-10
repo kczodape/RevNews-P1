a44544ca8f90abcb4925e86bf3e294754f61e310
kczodape <krunalzodape9@gmail.com>
2023-07-11 00:51:04 +0530

    added pagination

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GraphQLModule } from './graphql.module';
-import {  HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './components/login/login.component';
@@ -12,24 +12,30 @@ import { LandingComponent } from './components/landing/landing.component';
 import { IndexComponent } from './components/index/index.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatDialog, MatDialogRef, MatDialogModule} from '@angular/material/dialog';
-import {MatIconModule} from '@angular/material/icon'
-import {MatSelectModule} from '@angular/material/select';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import {
+  MatDialog,
+  MatDialogRef,
+  MatDialogModule,
+} from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { DeletedialogComponent } from './components/deletedialog/deletedialog.component';
 import { ResetdialogComponent } from './components/resetdialog/resetdialog.component';
 import { UpdatedialogComponent } from './components/updatedialog/updatedialog.component';
 import { ArticlesComponent } from './components/articles/articles.component';
-import {MatMenuModule} from '@angular/material/menu';
+import { MatMenuModule } from '@angular/material/menu';
 
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+// import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+// import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { MatPaginatorModule } from '@angular/material/paginator'; // Import MatPaginatorModule
 
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http);
-}
+
+// export function HttpLoaderFactory(http: HttpClient) {
+//   return new TranslateHttpLoader(http);
+// }
 
 @NgModule({
   declarations: [
@@ -43,20 +49,21 @@ export function HttpLoaderFactory(http: HttpClient) {
     DeletedialogComponent,
     ResetdialogComponent,
     UpdatedialogComponent,
-    ArticlesComponent
+    ArticlesComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     GraphQLModule,
     HttpClientModule,
-    TranslateModule.forRoot({
-      loader:{
-        provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [HttpClient],
-      },
-    }),
+    MatPaginatorModule ,
+    // TranslateModule.forRoot({
+    //   loader:{
+    //     provide: TranslateLoader,
+    //     useFactory: HttpLoaderFactory,
+    //     deps: [HttpClient],
+    //   },
+    // }),
     FormsModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
@@ -68,6 +75,6 @@ export function HttpLoaderFactory(http: HttpClient) {
     MatMenuModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/articles/articles.component.ts
+++ src/app/components/articles/articles.component.ts
@@ -1,57 +1,43 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ArticleService } from 'src/app/services/article.service';
 import { SearchService } from 'src/app/services/search.service';
 import * as moment from 'moment';
-import { NewsService } from 'src/app/services/news.service';
-import { SharedServiceService } from 'src/app/services/shared-service.service';
-// import { TranslationService } from 'src/app/services/translation.service';
 declare var google: any; // Declare the 'google' variable
 
 @Component({
   selector: 'app-articles',
   templateUrl: './articles.component.html',
-  styleUrls: ['./articles.component.scss'],
+  styleUrls: ['./articles.component.scss']
 })
 export class ArticlesComponent implements OnInit {
-  @ViewChild(ArticlesComponent) articlesComponent!: ArticlesComponent;
-
-  filterArticlesByLanguage(language: string) {
-    throw new Error('Method not implemented.');
-  }
-  public articles: any[] = [
-    // More articles...
-  ];
-
-  filteredArticles: any[] = [];
-  searchKeyword: string = '';
-  keyword: any[] = [];
+  public articles: any[] = [];
+  public filteredArticles: any[] = [];
+  public searchKeyword: string = '';
+  public keyword: any[] = [];
   public searchQuery: string = '';
-  category: string = '';
-  country: string | any;
+  public category: string = '';
+  public country: string | any;
+
+  public currentPage: number = 0;
+  public pageSize: number = 5;
+  public totalItems: number = 0;
 
   constructor(
     private articleService: ArticleService,
-    private searchService: SearchService,
-    private newsService: NewsService,
-    // private translationService: TranslationService
+    private searchService: SearchService
   ) {}
 
   ngOnInit() {
     this.articleService.selectedCountry$.subscribe((selectedCountry) => {
-      this.fetchArticles(
-        selectedCountry,
-        this.articleService.getSelectedCategory()
-      );
       this.country = selectedCountry;
+      this.fetchArticles(selectedCountry, this.articleService.getSelectedCategory());
     });
 
     this.articleService.selectedCategory$.subscribe((selectedCategory) => {
-      this.fetchArticles(
-        this.articleService.getSelectedCountry(),
-        selectedCategory
-      );
       this.category = selectedCategory;
+      this.fetchArticles(this.articleService.getSelectedCountry(), selectedCategory);
     });
+
     this.articleService
       .getArticles(
         this.articleService.getSelectedCountry(),
@@ -61,27 +47,22 @@ export class ArticlesComponent implements OnInit {
         this.articles = data.articles;
         this.filterArticles();
       });
+
     this.searchService.getSearchKeyword().subscribe((keyword) => {
       this.searchKeyword = keyword;
       this.filterArticles();
     });
 
-    // this.translationService.chnageLanguage('en');
-  }
-
-  public translateTo(event: any): void {
-    const language = event.target.value;
-    // this.translationService.chnageLanguage(language);
+    this.filterArticles();
   }
 
   fetchArticles(country: string, category: string) {
-    this.articleService
-      .getArticles(country, category)
-      .subscribe((data: any) => {
-        this.articles = data.articles;
-        this.filterArticles();
-      });
+    this.articleService.getArticles(country, category).subscribe((data: any) => {
+      this.articles = data.articles;
+      this.filterArticles();
+    });
   }
+
   filterArticles() {
     if (this.searchKeyword) {
       this.filteredArticles = this.articles.filter((article) =>
@@ -90,7 +71,15 @@ export class ArticlesComponent implements OnInit {
     } else {
       this.filteredArticles = this.articles;
     }
+
+    this.totalItems = this.filteredArticles.length;
   }
+
+  getPaginatedArticles(): any[] {
+    const startIndex = this.currentPage * this.pageSize;
+    return this.filteredArticles.slice(startIndex, startIndex + this.pageSize);
+  }
+
   formatDate(dateString: string): string {
     const dateObj = moment(dateString);
     if (dateObj.isValid()) {
@@ -99,6 +88,7 @@ export class ArticlesComponent implements OnInit {
       return 'Invalid Date';
     }
   }
+
   public performSearch(): void {
     // Filter the articles based on the search query
     this.filteredArticles = this.articles.filter(
@@ -107,6 +97,8 @@ export class ArticlesComponent implements OnInit {
         article.date.toISOString().includes(this.searchQuery) ||
         article.author.toLowerCase().includes(this.searchQuery.toLowerCase())
     );
+    this.currentPage = 0;
+    this.totalItems = this.filteredArticles.length;
   }
 
   public truncateWord(title: string, limit: number): string {
@@ -117,10 +109,15 @@ export class ArticlesComponent implements OnInit {
       return title;
     }
   }
-  public googleTranslateElementInit(){
+
+  public onPageChanged(page: number): void {
+    this.currentPage = page;
+  }
+
+   public googleTranslateElementInit() {
     new google.translate.TranslateElement(
-      {pageLanguage: 'en'},
+      { pageLanguage: 'en' },
       'google_translate_element'
-  );
+    );
   }
 }
